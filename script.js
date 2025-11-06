8e41859c77e9f35a29db1c5458f37f60fd596801
gurappadih2931 <gurappadih2931@my.sduhsd.net>
2025-11-06 06:37:28 +0000

    the actual last one

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function play(){
     }
     answer = Math.floor(Math.random()*level)+1;
     msg.textContent = name1 + " guess a number from 1 to " + level; 
-    guess.placeholder = answer;
+    //guess.placeholder = answer;
     score = 0;
     //let current-time = 
 }
